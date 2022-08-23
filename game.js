981d5b8b9f6eb54b696cdf94088cc213895fef7c
rieramos <angelrieramos@protonmail.com>
2022-08-23 11:31:07 -0400

    feature: responsive canvas display

--- game.js
+++ game.js
@@ -3,8 +3,16 @@ const game = canvas.getContext('2d')
 
 window.addEventListener('load', startGame)
 
-function startGame() { let canvasSize = window.innerHeight * 0.75;
+function startGame() { let canvasSize;
+
+    if (window.innerHeight > window.innerWidth){
+        canvasSize = window.innerWidth * 0.8;
+    } else {
+        canvasSize = window.innerHeight * 0.8;
+    }
 
     canvas.setAttribute('width',canvasSize);
     canvas.setAttribute('height',canvasSize);
+
+    window.innerWidth
 }
