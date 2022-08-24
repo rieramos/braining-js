d8913133de4ddd9f2fe96f5773fb7650475cfad9
rieramos <angelrieramos@protonmail.com>
2022-08-23 21:50:10 -0400

    code tweaks optimization

--- game.js
+++ game.js
@@ -4,12 +4,28 @@ const game = canvas.getContext('2d')
 function log(message) { console.log("[braining] " + message); }
 function warn(message) { console.warn("[braining] " + message); }
 
-window.addEventListener('load', startGame)
-window.addEventListener('resize', startGame)
+window.addEventListener('load', main)
+window.addEventListener('resize', main)
 
-function startGame() {
-
+function canvasInterface(){
+    //vars
     let canvasSize;
+
+// funcs
+    //usage: canvasInterfaceIcons
+    function canvasInterfaceIcons() { const elementsSize = canvasSize / 3;
+
+        game.font = elementsSize + 'px Verdana';
+        game.textAlign = 'end';
+    
+        for (let y = 1; y < 4; y++) {
+            for (let x = 1; x < 4; x++) {
+                game.fillText(arrows['up'],elementsSize * x, elementsSize * y);
+            }
+        }
+
+        log('canvas size: ' + canvasSize + ' | icons size: ' + elementsSize);
+    }
 
     if (window.innerHeight > window.innerWidth){
         canvasSize = window.innerWidth * 0.8;
@@ -17,19 +33,13 @@ function startGame() {
         canvasSize = window.innerHeight * 0.8;
     }
 
-    canvas.setAttribute('width',canvasSize);
-    canvas.setAttribute('height',canvasSize);
+    // set canvas composition
+    canvas.setAttribute('width',canvasSize); canvas.setAttribute('height',canvasSize);
 
-    const elementsSize = canvasSize / 3;
+    // set icons
+    canvasInterfaceIcons();
+}
 
-    log('canvas size: ' + canvasSize + ' | icons size: ' + elementsSize);
-
-    game.font = elementsSize + 'px Verdana';
-    game.textAlign = 'end';
-
-    for (let y = 1; y < 4; y++) {
-        for (let x = 1; x < 4; x++) {
-            game.fillText(arrows['up'],elementsSize * x, elementsSize * y);
-        }
-    }
+function main(){ 
+    canvasInterface();
 }
