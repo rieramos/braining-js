9c4c5b941bd4841a7ec22531ffb23a4c70018bbd
rieramos <angelrieramos@protonmail.com>
2022-09-16 18:39:54 -0300

    3feature: set wildcards (directions emojis) module(s) move to game.js
    to have available the refresh

--- game.js
+++ game.js
@@ -8,7 +8,8 @@ attention to details, speed, and ability to ignore competing information
 From: Consulting Inc, M. (2012). Mind Games (Version 3.4.5) [Mobile App]. Play Store. https://play.google.com/store/apps/details?id=mindware.mindgames
 */
 
-//funcs (unralted)
+let wildcards = { 'X':'', 'A':'', }
+
 function log(message) { debug && console.log("[braining][main] " + message); }
 function warn(message) { debug && console.warn("[braining][main] " + message); }
 
@@ -18,25 +19,27 @@ canvas = document.querySelector('#game'); game = canvas.getContext('2d');
 //events
 window.addEventListener('load', main); window.addEventListener('resize', main);
 
-function verifiedKey(keyID){log(keyID); (+keyID === +wildcards['A']) ? console.log('+') : console.log('-'); main()};
+function verifiedKey(keyID){log(keyID); (+keyID === +wildcards['A']) ? main() : console.log('-')};
 
 //usage: canvasInterface
 function canvasInterface(){
+
     //usage: canvaSize
     function canvaSize(){
-        const size = (function(){ let height = window.innerHeight; let width = window.innerWidth;
+
+        const element = (function(){ let height = window.innerHeight; let width = window.innerWidth;
             
             return height < width ? (height * 0.8) : (width * 0.8);
 
-        })(); log('canvas size: ' + Math.floor(size));
+        })(); log('canvas element: ' + Math.floor(element));
         
-        canvas.setAttribute('width',size); canvas.setAttribute('height',size); return size
+        canvas.setAttribute('width',element); canvas.setAttribute('height',element); return element
 
     }; let difficulty = 5 //temporary cells box n
     
-    const element = canvaSize() / difficulty; log('icons size: ' + element + 'px');
+    const size = canvaSize() / difficulty; log('icons size: ' + size + 'px');
 
-    game.font = element + 'px Verdana' ; game.textAlign = 'end';
+    game.font = size + 'px Verdana' ; game.textAlign = 'end';
 
     function assignIcons(n) { log(maps[n])
         
@@ -44,25 +47,40 @@ function canvasInterface(){
                            .map(a=>a.split(""));
 
         if (debug) {
-            for (let i = 0; i < difficulty; ++i) { const arr = map[i]; log('map[' + i + ']: ' + arr) }
+            //for (let i = 0; i < difficulty; ++i) { const arr = map[i]; log('map[' + i + ']: ' + arr) }
             for (const wildcard in wildcards) { log('wildcards[' + wildcard + ']: ' + wildcards[wildcard]) }; log("wildcards[-]: ")
         };
 
         map.forEach((n, x) => {
-            n.forEach((symbol, y) => { item=symbols[wildcards[symbol]]
+            n.forEach((symbol, y) => { item=emojis[wildcards[symbol]]
 
                 typeof item === 'undefined' && ( item = ' ');
 
-                game.fillText(item, [element * (y + 1)], [element * (x + 1)])
+                game.fillText(item, [size * (y + 1)], [size * (x + 1)])
             });
         })
     }; assignIcons(Math.floor(Math.random()*maps.length));
 };
 
-function main() { console.clear()
-    canvasInterface();
-    //other
-};
+function setWildcards(){ let directions = [ 37, 38, 39, 40 ]
+
+    for (const wildcard in wildcards) {
+
+        wildcards[wildcard] = (function(){ log('directions: ' + directions)
+
+            direction = directions[Math.floor(Math.random()*directions.length)];
+
+            switch (wildcard) {
+
+                case 'A': if (Math.floor(Math.random()*1) === 1) { break }; //choose posible a repeat option or, a new (just to get it some randomness)
+
+                default: { index=directions.indexOf(direction); (index !== -1) && directions.splice(index, 1) };
+
+            };return direction
+        })();
+    }
+
+}; function main() { console.clear(); setWildcards(); canvasInterface() };
 
 document.addEventListener("keydown", function( event ) { verifiedKey(event.which); //let key = event.which
 });
